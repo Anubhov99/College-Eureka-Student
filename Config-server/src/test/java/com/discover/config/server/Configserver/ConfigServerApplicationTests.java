891d44acde3cdedb304692a947c0169aeaf381f0
Anubhov Menon <m.anubhov99@gmail.com>
2022-04-19 10:52:18 +0530

    Config Server

---
+++ Config-server/src/test/java/com/discover/config/server/Configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discover.config.server.Configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
