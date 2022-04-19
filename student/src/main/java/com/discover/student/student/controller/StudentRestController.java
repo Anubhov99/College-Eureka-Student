b5b57df87ee9d95847752ac6d9cc8e72fe22ce5d
Anubhov Menon <m.anubhov99@gmail.com>
2022-04-19 10:51:47 +0530

    Config Server

--- student/src/main/java/com/discover/student/student/controller/StudentRestController.java
+++ student/src/main/java/com/discover/student/student/controller/StudentRestController.java
@@ -3,6 +3,8 @@ package com.discover.student.student.controller;
 import com.discover.student.student.model.College;
 import com.discover.student.student.service.CollegeService;
 import com.netflix.discovery.converters.Auto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,11 @@ import java.util.List;
 public class StudentRestController {
     @Autowired
     private CollegeService collegeService;
+    Logger logger = LoggerFactory.getLogger(StudentRestController.class);
 
     @GetMapping("/allColleges")
     public List<College> getAllColleges() {
+        logger.trace("...SUCCESSFULLY REACHED GET ALL COLLEGES METHOD...");
         return collegeService.getCollege();
     }
 }
